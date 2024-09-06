21eec2b9f00b5ffa83463c24b08946390307d3e6
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2024-09-06 23:59:25 +0300

    Your commit message here

--- src/js/about.js
+++ src/js/about.js
@@ -1,7 +1,25 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+const swiperBtn = document.querySelector('.swiper-button');
 
 // ACCORDION
-new Accordion('.accordion-container');
+const accordionAbout = new Accordion('.accordion-container', {
+  duration: 700,
+  showMultiple: true,
+});
+accordionAbout.open(0);
 
 // SWIPER
+
+const swiper = new Swiper('.about-swiper', {
+  loop: true,
+  slidesPerView: 2,
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
+});
