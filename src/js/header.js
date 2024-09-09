c27504c60208444502c320849521be1e4acc8110
Misha Kopiluk <misakopiluk2019@gmail.com>
2024-09-09 14:46:46 +0300

    header correction

--- src/js/header.js
+++ src/js/header.js
@@ -1,17 +1,60 @@
 const menu = document.querySelector(".burger-modal-window");
-const btnMenu = document.querySelector(".modal-window-btn");
-const iconBtn = document.querySelector(".icon-burger-btn");
-const menuEl = document.querySelector(".hidden-menu");
+const closeButton = document.querySelector(".modal-window-btn");
+const burgerButton = document.querySelector(".icon-burger-btn");
+const menuEl = document.querySelector(".modal-container");
+const modalWindowMenu = document.querySelector(".mobile-menu");
 const btn = document.querySelector(".btn");
 
-const fiilEl = () => {
-    menuEl.classList.toggle("is-activation")
+
+const menuItem = () => {
+  menuEl.classList.toggle("is-activation")  
+  
+  const openMenu = btn.getAttribute('aria-expanded') === "true" || false;
+  const links = menuEl.querySelectorAll("a[href*='#']");
+    if(!openMenu) {
+      links.forEach(link => {
+      link.addEventListener("click", menuItem)
+    })
+    return;
+  }
+  
+  for (let link of links) {
+    link.addEventListener("click", event => {
+      event.preventDefault();
+      const bloclId = link.getAttribute("href")
+      document.querySelector("" + bloclId).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      })
+    })
+  }
 }
 
-const el = () => {
+const openingAndClosingTheModalWindow = () => {
+  const openMenu = burgerButton.getAttribute("aria-expanded") === "true" || false;
+  const links = document.querySelectorAll('a[href*="#"]')
+  
     menu.classList.toggle("is-open")
+    if(!openMenu) {
+      links.forEach(link => {
+      link.addEventListener("click", openingAndClosingTheModalWindow);
+    })
+    return;
+    }
+
+  
+  for (let link of links) {
+    link.addEventListener("click", event => {
+      event.preventDefault();
+      const bloclId = link.getAttribute("href")
+      document.querySelector("" + bloclId).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      })
+    })
+  }
 }
 
-btn.addEventListener("click", fiilEl);
-iconBtn.addEventListener("click", el);
-btnMenu.addEventListener("click", el);
+btn.addEventListener("click", menuItem);
+burgerButton.addEventListener("click", openingAndClosingTheModalWindow);
+closeButton.addEventListener("click", openingAndClosingTheModalWindow);
