5e2b6fa3c4bced9a026114283c7acc990df8dd1e
ihor-voznyi <haiku.kiev@gmail.com>
2024-09-09 16:21:41 +0300

    update html css faq.js

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,14 +1,12 @@
-// Імпорт бібліотеки Accordion  
-import Accordion from 'accordion-js';  
+ import Accordion from 'accordion-js';  
 import 'accordion-js/dist/accordion.min.css';  
 
-// Ініціалізація акордеона з потрібними класами  
+ 
 document.addEventListener('DOMContentLoaded', () => {  
     const accordionFaq = new Accordion('.accordion-faq', {  
         duration: 300,  
-        showMultiple: false, // Встановлення 'false' відкриває лише один елемент за раз  
+        showMultiple: false,  
     });  
-
-    // Опціонально: відкрити перший елемент відразу  
+ 
     accordionFaq.open(0);  
 });  
