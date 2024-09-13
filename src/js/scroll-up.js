e5b1b4dbcbd1468beaae6c7652bc83299e61d5e8
Ivan_Bog <24term@gmail.com>
2024-09-13 10:53:58 +0300

    edit scroll

--- src/js/scroll-up.js
+++ src/js/scroll-up.js
@@ -1,5 +1,11 @@
 window.addEventListener('scroll', function () {
   var scrollButton = document.querySelector('.top');
+  
+  // Перевіряємо, чи заблоковано скрол
+  if (document.body.classList.contains('no-scroll')) {
+    return; // Якщо скрол заблоковано, зупиняємо виконання
+  }
+  
   if (window.scrollY > 1000) {
     scrollButton.style.display = 'flex';
   } else {
@@ -7,8 +13,17 @@ window.addEventListener('scroll', function () {
   }
 });
 document.querySelector('.top').addEventListener('click', function () {
+  // Тимчасово розблоковуємо скрол
+  document.body.classList.remove('no-scroll');
+
+  // Прокручуємо сторінку вгору
   window.scrollTo({
     top: 0,
     behavior: 'smooth',
   });
+
+  // Знову блокуємо скрол через невеликий час після прокрутки
+  setTimeout(() => {
+    document.body.classList.add('no-scroll');
+  }, 500); // Виставляємо час відповідно до швидкості прокрутки
 });
